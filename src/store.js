d3b914d129fdbaac54a286f81c1b054802b4a510
Sanket Chavan <sanketc2102@gmail.com>
2026-07-07 22:46:21 +0530

    impliment store in RTK way

--- src/store.js
+++ src/store.js
@@ -1,14 +1,12 @@
-import { combineReducers, createStore } from "redux";
 import accountReducer from "./features/accountSlcie/accountSlice";
 import customerReducer from "./features/customerSlice/customerSlice";
-import { applyMiddleware } from "redux";
-import { thunk } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 
-const reducers = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-const store = createStore(reducers, applyMiddleware(thunk));
 
 export default store;
